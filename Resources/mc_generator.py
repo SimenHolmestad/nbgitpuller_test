36e0dc307e6559a74c78601c728645d12e8db9c6
Tobias I. Ørstad <tobiasio@ntnu.no>
2019-07-12 10:41:31 +0200

    Complete more exam exercises, add resources

--- Resources/mc_generator.py
+++ Resources/mc_generator.py
@@ -20,7 +20,7 @@ def generate_mc(answers,question,answer,custom_feedback=[],shuffle=True):
     description='',
     layout=widgets.Layout(width='1500px'),
     disabled=False)
-    
+    answer=answers.index(answer_string)
     print('\033[1m',question,'\033[0m')
     button = widgets.Button(description="Sjekk svaret")
     display(alternativ)
@@ -29,20 +29,53 @@ def generate_mc(answers,question,answer,custom_feedback=[],shuffle=True):
         a = alternativ.value
         if(a==answer_string):
             color = '\x1b[6;30;42m' + "Riktig." + '\x1b[0m' +"\n" #fargen blir da grønn
+            if(len(custom_feedback)==len(answers)):
+                color += custom_feedback[answer]
         else:
             color = '\x1b[5;30;41m' + "Feil. " + '\x1b[0m' +"\n" #ellers blir fargen rød
         with out:
             clear_output()
         with out:
             print(color)
-            if(len(custom_feedback)==len(answers)):
-                print(custom_feedback[a-1])
+            
       
     display(out)
     button.on_click(check_answer)
     
+def generate_mc_no_answer(answers,question):
+    out = widgets.Output()
+    alternativ = widgets.RadioButtons(
+    options=answers,
+    description='',
+    layout=widgets.Layout(width='1500px'),
+    disabled=False)
+    
+    print('\033[1m',question,'\033[0m')
+    display(alternativ) 
+    display(out)
+    return alternativ
+
+def generate_multiple_no_answer(filename): #Tar inn tekst med linjer på formatet spørsmål£alternativ1£alternativ2osv
+    f = open(filename,"r",encoding="UTF-8")
+    lines = f.readlines()
+    f.close()
+    #buttons = []
+    for line in lines:
+        line = line.split("£")
+        question = line[0]
+        answers = line[1:]
+        generate_mc_no_answer(answers,question)
+        #buttons.append(generate_mc_no_answer(answers,question))
+   # button = widgets.Button(description="Lagre svar")
+   # display(button)
+    """def save_answers(b):
+        saved_answers = []
+        for radiobutton in buttons:
+            saved_answers.append(radiobutton.value)
+    button.on_click(save_answers)"""
+    
 def generate_multiple(filename): #Tar inn tekst på formatet spørsmål, antall alternativer, [alternativer], rett svar (tall)
-    f = open(filename,"r")
+    f = open(filename,"r",encoding="UTF-8")
     lines = f.readlines()
     f.close()
     for line in lines:
@@ -55,7 +88,7 @@ def generate_multiple(filename): #Tar inn tekst på formatet spørsmål, antall 
         
         
 def generate_multiple1(filename): #Tar inn tekst på formatet spørsmål, rett svar (streng), [andre alternativer]
-    f = open(filename,"r")
+    f = open(filename,"r",encoding="UTF-8")
     lines = f.readlines()
     f.close()
     for line in lines:
@@ -74,7 +107,6 @@ def generate_multiple3(filename): #her er førstelinje alle svarene på form f.e
     correct_answers=[]
     for letter in lines[0]:
         correct_answers.append(ord(letter) - 96)
-    print(correct_answers)
     for i in range(1,len(lines)-1):
         line = lines[i].split("£")
         question = line[0]
