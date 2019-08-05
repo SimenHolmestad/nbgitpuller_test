32106133b21a94ff438aa90f15df30c9e54c546a
Simen Holmestad <simen@holmestad.com>
2019-08-05 08:59:29 +0200

    Started a terminal tutorial

---
+++ Tutorials/terminal-labyrinten/skogen/bortover_en_sti/inn_i_en_hule/monster.py
@@ -0,0 +1,7 @@
+from time import sleep
+print("DRØØNNN... Hele hulen rister...")
+sleep(2)
+navn = input("Hvem er du som skriver shell-kommandoer i min terminal? ")
+print("Å hei, " + navn + ". Det var koselig med litt selskap. Det er litt mørkt her inne")
+sleep(4)
+print("Det første hemmlige passordet er: Sure sokker")
